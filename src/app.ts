72aeef1d351f7ac15db8b4d1f20183547ca0a2b4
Mikita <nikitarakovich61@gmail.com>
2025-02-08 17:09:01 +0100

    cors conf

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import routesConfig from "./routes/routes";
+import cors from "cors";
 const app: Application = express();
 
+app.use((req: Request, res: Response, next) => {
+        next();
+}, cors({maxAge: 864600}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 routesConfig(app);
 
