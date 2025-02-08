14f18888841a5ab5a6ff7a6695c6afe18e06005e
Mikita <nikitarakovich61@gmail.com>
2025-02-08 14:32:31 +0100

    minor

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -4,7 +4,6 @@ const router = Router();
 
 router.get("/api/superheroes", superheroController.getAllSuperheroesHumilityScoreDescending);
 router.post("/api/superheroes", superheroController.createNewSuperhero);
-router.get("*", (req, res) => res.status(404).send("Not found"));
 
 export default (app: Application) => {
     app.use(router);
