45bd9c7a419204a5746174f08ba15adb78ea24cf
Mikita <nikitarakovich61@gmail.com>
2025-02-08 14:28:40 +0100

    minor

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,8 +2,8 @@ import {Application, Router} from "express";
 import superheroController from "../controllers/superheroController";
 const router = Router();
 
-router.get("/superheroes", superheroController.getAllSuperheroesHumilityScoreDescending);
-router.post("/superheroes", superheroController.createNewSuperhero);
+router.get("/api/superheroes", superheroController.getAllSuperheroesHumilityScoreDescending);
+router.post("/api/superheroes", superheroController.createNewSuperhero);
 router.get("*", (req, res) => res.status(404).send("Not found"));
 
 export default (app: Application) => {
